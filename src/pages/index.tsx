b853c4a7980323f62e0be434dfa2992779a49882
Jonatas Felipe de Souza <jonatasfelipe2@hotmail.com>
2021-09-06 16:10:40 -0300

    Console log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,38 @@
-import styles from './home.module.scss'
+import styles from "./home.module.scss";
 
-import { Container } from '../components/Container'
-import { GetStaticPaths, GetStaticProps } from 'next'
-import { getPrismicClient } from '../services/prismic'
+import { Container } from "../components/Container";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { getPrismicClient } from "../services/prismic";
 
-import { RichText } from 'prismic-dom'
-import { getHeaderTitle } from '../utils/getHeaderTitle'
+import { RichText } from "prismic-dom";
+import { getHeaderTitle } from "../utils/getHeaderTitle";
 
 type Content = {
   title: string;
   content: string;
   urlBackground: string;
-}
+};
 
 interface ContentsProps {
   contentPage: Content;
-  headerTitle: string
+  headerTitle: string;
 }
 
 export default function Home({ contentPage, headerTitle }: ContentsProps) {
+  console.log(headerTitle);
 
   return (
-    <Container urlImg={contentPage.urlBackground} headerTitle={headerTitle}>      
+    <Container urlImg={contentPage.urlBackground} headerTitle={headerTitle}>
       <section className={styles.contentSection}>
         <div className={styles.content}>
           <p>{contentPage.title}</p>
           <h1>
-            {contentPage.content[0]} 
+            {contentPage.content[0]}
             <span> </span> {contentPage.content[1]}
             <span> </span> {contentPage.content[2]}
             <span> </span> {contentPage.content[3]}
             <span> </span> {contentPage.content[4]}
-            <span> </span> {contentPage.content[5]} 
+            <span> </span> {contentPage.content[5]}
             <span> </span> {contentPage.content[6]}
             <span> </span> {contentPage.content[7]}
             <span>
@@ -41,33 +42,35 @@ export default function Home({ contentPage, headerTitle }: ContentsProps) {
           </h1>
           <div className={styles.line}></div>
         </div>
-      </section> 
+      </section>
     </Container>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const headerTitle = await getHeaderTitle()
+  const headerTitle = await getHeaderTitle();
 
-  const prismic = getPrismicClient()
+  const prismic = getPrismicClient();
 
-  const response = await prismic.getByUID('texto_da_pagina', 'texto-pagina-inicial', {
-
-  })
+  const response = await prismic.getByUID(
+    "texto_da_pagina",
+    "texto-pagina-inicial",
+    {}
+  );
 
   const contentPage = {
     title: RichText.asText(response.data.titulo),
-    content: RichText.asText(response.data.texto).split(' '),
-    urlBackground: response.data.background.url
-  }
+    content: RichText.asText(response.data.texto).split(" "),
+    urlBackground: response.data.background.url,
+  };
 
-  console.log(headerTitle)
+  console.log(headerTitle);
 
   return {
     props: {
       contentPage,
-      headerTitle
+      headerTitle,
     },
     revalidate: 60 * 60 * 12, //12 hours
-  }
-}
+  };
+};
